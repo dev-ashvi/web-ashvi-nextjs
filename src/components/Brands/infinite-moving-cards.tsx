7cb2b2f9082160e340f7bc82ebfca885c1a2674e
Ashvi <ashvi.dev@gmail.com>
2025-01-26 17:13:40 +0530

    fix

--- src/components/Brands/infinite-moving-cards.tsx
+++ src/components/Brands/infinite-moving-cards.tsx
@@ -133,7 +133,7 @@ export const InfiniteMovingCards = ({
             >
                {item.description}
             </CardItem>
-            <CardItem translateZ="100" className="w-full mt-4">
+            <CardItem translateZ={100} className="w-full mt-4">
               <Image
                 src={item.image}
                 height="100"
@@ -154,7 +154,6 @@ export const InfiniteMovingCards = ({
                 translateZ={20}
                 as={Link}
                 href={item.url}
-                target="__blank"
                 className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
               >
                   Visit now →
