5eda3176bb3d79cf818dadffee28f4df76c05e1d
Ashvi <ashvi.dev@gmail.com>
2025-01-26 17:03:07 +0530

    fix

--- src/components/Brands/3d-card.tsx
+++ src/components/Brands/3d-card.tsx
@@ -92,12 +92,12 @@ interface CardItemProps extends HTMLAttributes<HTMLElement> {
   as?: React.ElementType;
   children: ReactNode;
   className?: string;
-  translateX?: string;
-  translateY?: string;
-  translateZ?: string;
-  rotateX?: string;
-  rotateY?: string;
-  rotateZ?: string;
+  translateX?: number;
+  translateY?: number;
+  translateZ?: number;
+  rotateX?: number;
+  rotateY?: number;
+  rotateZ?: number;
 }
 
 export const CardItem: React.FC<CardItemProps> = ({
--- src/components/Brands/infinite-moving-cards.tsx
+++ src/components/Brands/infinite-moving-cards.tsx
@@ -73,14 +73,14 @@ export const InfiniteMovingCards = ({
           <CardContainer key= {item.id || idx} className="inter-var">
           <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
             <CardItem
-              translateZ="50"
+              translateZ={50}
               className="text-xl font-bold text-neutral-600 dark:text-white"
             >
               {item.name}
             </CardItem>
             <CardItem
               as="p"
-              translateZ="60"
+              translateZ={60}
               className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
             >
               {item.description}
@@ -122,14 +122,14 @@ export const InfiniteMovingCards = ({
           <CardContainer key= {item.id || idx} className="inter-var">
           <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
             <CardItem
-              translateZ="50"
+              translateZ={50}
               className="text-xl font-bold text-neutral-600 dark:text-white"
             >
               {item.name}
             </CardItem>
             <CardItem
               as="p"
-              translateZ="60"
+              translateZ={60}
               className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
             >
                {item.description}
