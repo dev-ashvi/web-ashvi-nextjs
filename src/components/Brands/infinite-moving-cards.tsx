3d3fa8c7c41db7ea13250f328657a887367caf35
Ashvi <ashvi.dev@gmail.com>
2025-01-26 17:12:00 +0530

    fix

--- src/components/Brands/infinite-moving-cards.tsx
+++ src/components/Brands/infinite-moving-cards.tsx
@@ -106,7 +106,6 @@ export const InfiniteMovingCards = ({
                 translateZ={20}
                 as={Link}
                 href={item.url}
-                target="__blank"
                 className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
               >
                 Visit now →
